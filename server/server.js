3790f4f5f36764b0939861c81b277ba395f4f6ff
Iliass Hilmi <iliasshlm@gmail.com>
2020-07-19 18:16:02 +0200

    Add index route

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const indexRouter = require('./routes/index');
 
-
+app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
