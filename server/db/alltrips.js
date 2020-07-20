b5e00f2588785ca1d7829f70f0bc6fe9898b88c0
Iliass Hilmi <inf2766@hs-worms.de>
2020-07-20 12:03:23 +0100

    Add getTrips according to departure, destination and input date

--- server/db/alltrips.js
+++ server/db/alltrips.js
@@ -11,4 +11,42 @@ module.exports = {
             return db.collection('trip').find().toArray();
         });
     },
+    // get custom trips
+    getFromToTrip: function(from, to, date) {
+        let dateString = date;
+        let newDate = new Date(dateString);
+        //add a day to the date
+        newDate.setDate(newDate.getDate() + 1);
+        let y = newDate.getFullYear(),
+            m = newDate.getMonth() + 1, // january is month 0 in javascript
+            d = newDate.getDate();
+        newDate = function(val) { var str = val.toString(); return (str.length < 2) ? "0" + str : str};
+        dateString = [y, newDate(m), newDate(d)].join("-");
+
+        return client.connect().then((client)=>{
+            let db = client.db('data')
+            return db.collection('trip').find({
+                $and: [
+                    {
+                        departure: from
+                    },
+                    {
+                        arrival: to
+                    },
+                    { startTime: {
+                            $gte: date,
+                            $lt:  dateString
+                        }
+                    }
+                ]
+            }).toArray();
+        });
+    },
 };
+
+/*
+{ startTime: {
+                        $gte: '2020-10-01 00:00:00',
+                        $lt:  '2020-10-02 00:00:00'
+                      }
+                    } */
--- server/routes/trips.js
+++ server/routes/trips.js
@@ -10,4 +10,11 @@ router.get('/all', async function (req, res) {
     res.send(allTrips);
 });
 
+/* Get custom trips*/
+router.get('/trip', async function (req, res) {
+    const trip = await db.getFromToTrip("Mannheim","Frankfurt",'2020-10-01 00:00:00');
+    console.log(trip);
+    res.send(trip);
+});
+
 module.exports = router;
