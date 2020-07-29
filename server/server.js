efb3184cc3f33ab116426ae1d89d41a81a522544
Iliass Hilmi <inf2766@hs-worms.de>
2020-07-29 16:09:47 +0100

    Add Cors package and allow All CORS Requests

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const getAllTrips = require('./routes/trips');
 
+app.use(cors());
 app.use('/trips', getAllTrips);
 
 app.listen(port, () => {
